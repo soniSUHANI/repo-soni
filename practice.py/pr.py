2c4374f017af9c33221a1a95d51511ead5bdd85f
soniSUHANI <soni03suhani@gmail.com>
2022-10-15 19:59:51 +0530

    practice qn

---
+++ practice.py/pr.py
@@ -0,0 +1,10 @@
+# modify the above question to allow student to sit if he/she has medical cause Ask user
+#  if he/she has medical cause or not ('Y' or 'N')and print accordingly
+qn = str(input("Do you have medical cause: "))
+Y = "yes"
+N = "no"
+if qn == Y:
+    print("you are allowed to sit") 
+elif qn == N:
+    print("you are not allowed")
+    
